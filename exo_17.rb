3f5e4db37827a4714b9d032e4247594715a77af9
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-06 00:00:29 +0100

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -8,16 +8,18 @@ now = 2018
 
 #countup with no year
 loop do
-   if "#{now - countup_value}" == "#{countup_value - user_year_of_birth}"
+   if (now - countup_value == countup_value - user_year_of_birth)
       puts "il y a #{now - countup_value} ans, tu avais la moitié de l'age que tu as aujourd'hui !"
    else 
       puts "Il y a  #{now - countup_value} ans,  tu avais #{countup_value - user_year_of_birth} ans."
-      end
+   end
+   
    if countup_value == now
   break  # this will cause execution to exit the loop in 2017		
   end
-   countup_value +=1
-end
+   
+          countup_value +=1
+   end
 puts "Tu es vieux maintenant!"
 
    
