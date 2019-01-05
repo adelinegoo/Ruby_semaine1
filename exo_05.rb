47effc9b58379cca3bbd93a7e56d6b4cc6af3336
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 22:55:09 +0100

    Update exo_05.rb

--- exo_05.rb
+++ exo_05.rb
@@ -1,8 +1,22 @@
-number_of_hours_worked_per_day = 10
-number_of_days_worked_per_week = 5
-number_of_weeks_in_THP = 11
-number_of_minutes_in_an_hour = 60
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail :  #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
-puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "Et en secondes ?"
 
-puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "#{10 * 5 * 11 * 60*60}"
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts "#{3 + 2 < 5 - 7}"
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
