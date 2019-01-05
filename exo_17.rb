ce8d06dcd852c77674cc3d00125d36a7a70bb1cb
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:45:34 +0100

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -2,15 +2,16 @@
 puts "Merci de me donner votre année de naisssance."
 
 # core value
-user_year_of_birth = gets.chomp
+user_year_of_birth = gets.chomp.to_i
 user_year_of_birth.to_i.times do |i|
 a = user_year_of_birth.to_i
 
 # countup with no year  
    if "#{i + 1}" != "{a - i - 1}"
    	puts "il y a #{i + 1} ans tu avais #{a - i - 1} ans"
+   
     #  this year - countup_value = age
    else "#{i + 1}" == "#{a - i - 1}"
    		puts "il y a #{i + 1} ans, tu avais la moitié de l'age que tu as aujourd'hui !"
-
+   end
 end
