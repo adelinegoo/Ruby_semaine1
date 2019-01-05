d28c67131721baa2de6457537cdc7b5df0bc137d
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:23:30 +0100

    Update exo_11.rb

--- exo_11.rb
+++ exo_11.rb
@@ -1,13 +1,13 @@
 # exo_11.rb qui demande un nombre à l'utilisateur, puis qui écrira autant de fois "Salut, ça farte ?"
 
-puts "Merci de me donner un nombre au pif".
+puts "Merci de me donner un nombre au pif."
 user_number = gets.chomp
 
 i = 0
 loop do
   i += 1
   puts "Salut, ça farte ?"
-  if i = user_number.to_i
+  if i == user_number.to_i
     break       # this will cause execution to exit the loop
   end
 end
