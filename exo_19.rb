627c9189408409c1f2ebcc5c3cd9128d035c3896
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-06 00:16:57 +0100

    Update exo_19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -1,13 +1,11 @@
 #  exo_19.rb
 # email generator -pair number
 
-  emails = [01]
-number = while emails << [50]
-  rand(01...50)
+1=0
+mail_arr=[]
+while (i < 50)
+  1 +=1
+  number= rand(01...50)
+   mail_arr << "jean.dupont "+number.to_s+"@email.fr"
 end
-
-
-if emails << [50]
-   emails.push ["jean" + "." + "dupont" + "." +"#{number}" +  "@email.fr"] * 50
-end
-puts emails if emails.to_f%2==0
+puts mail_arr
