7fca0dda558660c77eb703ebc3264de84e377dc3
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:10:19 +0100

    Update exo_10.rb

--- exo_10.rb
+++ exo_10.rb
@@ -1,4 +1,6 @@
 puts "Bonjour, quelle est ton annee de naissance? "
-user_birthyear = gets.chomp
+user_birthyear = gets.chomp.to_i
 year_requested = 2017
-puts "Tu avais #{year_requested - user_birthyear} en 2017."
+
+puts "Tu avais: #{year_requested - user_birthyear} en 2017."
+
