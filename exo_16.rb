f26a2b748e3a08532ec468a0a73731fb38744240
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:44:07 +0100

    Update exo_16.rb

--- exo_16.rb
+++ exo_16.rb
@@ -2,15 +2,18 @@
 puts "Merci de me donner votre année de naisssance."
 
 # core value
-user_year_of_birth = gets.chomp
+user_year_of_birth = gets.chomp.ti_i
 countup_value = user_year_of_birth
 now = 2018
 
 #countup with no year
 loop do
-  puts "Il y a X ans, #{now - countup_value}  tu avais Y ans #{countup_value - user_year_of_birth}."
+  puts "Il y a  #{now - countup_value} ans,  tu avais #{countup_value - user_year_of_birth} ans."
   if countup_value == now
   break  # this will cause execution to exit the loop in 2018
+  end
 end
+
+countup_value +=1
 
 puts "Tu es vieux maintenant!"
