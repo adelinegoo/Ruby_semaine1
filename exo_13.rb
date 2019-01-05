feb73a0af6ad50926bc7ba902465b48b15eb6378
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:33:40 +0100

    Update exo_13.rb

--- exo_13.rb
+++ exo_13.rb
@@ -1,22 +1,18 @@
-# exo_13.rb
+# exo_13.rb Countup birthdate
 
 puts "Merci de me donner votre année de naisssance."
 user_year_of_birth = gets.chomp
-
-
 i = user_year_of_birth.to_i
+
 loop do
   i += 1
   puts " #{i}"
   if i == 2018
-  break  # this will cause execution to exit the loop in 2018
+     break  # this will cause execution to exit the loop in 2018
+  end
 end
-
 puts "C'est fini!"
   
-  # exo_12 Countup timer
 
-puts "Merci de me donner un nombre au pif."
-user_number = gets.chomp
 
   
