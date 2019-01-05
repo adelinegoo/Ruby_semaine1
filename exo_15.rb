9fcacdc73852f15c2a7493c1922714cf17059bee
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-05 23:42:18 +0100

    Update exo_15.rb

--- exo_15.rb
+++ exo_15.rb
@@ -1,17 +1,18 @@
 # exo_15.rb
 puts "Merci de me donner votre année de naisssance."
 user_year_of_birth = gets.chomp.to_i
-
+countup_value = user_year_of_birth
 
 # countup
-countup_value += 1
 loop do
   puts "Tu avais #{countup_value - user_year_of_birth} en #{countup_value}."
-# Age result per year
 
+  # Age result per year
   if countup_value == 2017
-    break  # this will cause execution to exit the loop in 2018
+    break  # this will cause execution to exit the loop in 2017
   end
+  
+  countup_value += 1
 end
 
 puts "Tu es vieux maintenant!"
